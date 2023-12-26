bf14d4ea2f4f5d67c293158d5fa1c719cb50fd36
Fabian1032908ru <fstiewe@aol.com>
2023-12-26 18:20:07 +0100

    Changed an error of generating the board

--- GameDict/TicTacToeGame.cpp
+++ GameDict/TicTacToeGame.cpp
@@ -302,7 +302,7 @@ private:
     }
 
     static array<array<string , 3>, 3> generateBoard() {
-        array<array<string , 3>, 3> board{};
+        array<array<string , 3>, 3> board;
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 board[i][j] = " ";
